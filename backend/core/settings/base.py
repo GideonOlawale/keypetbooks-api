1603317b2003e2bafee0b65ee5ea753635516abb
Hudeh <henryudeh@hudehtech.com>
2021-03-05 18:05:20 +0100

    adding export

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -91,7 +91,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'core/static')
 ]
 MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
