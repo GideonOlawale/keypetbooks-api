d2874b87d5c1fcbaffcfc3e56e7bcd121c613284
Hudeh <henryudeh@hudehtech.com>
2021-03-08 13:46:48 +0100

    exporting to csv

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,19 +1,19 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
+from rest_framework.settings import api_settings
+from rest_framework_csv import renderers as r
 from rest_framework import  permissions
+from django.http import HttpResponse
+from rest_framework.response import Response
 from .models import CsvModel, CsvModelResouces
 
 
 class CSVApi(APIView):
 
     permission_classes = [permissions.AllowAny]
-    renderer_classes = [JSONRenderer]
+    renderer_classes = (r.CSVRenderer, ) + tuple(api_settings.DEFAULT_RENDERER_CLASSES)
     def get(self, request, format=None):
-        csv_data = CsvModelResouces()
-        query_africa =  CsvModel.objects.filter(continent='Africa')
-        africa_dataset = csv_data.export(query_africa)
-        response = Response({
-        	'africa':africa_dataset.csv
-        	})
+        csv_resource = CsvModelResouces()
+        dataset = csv_resource.export()
+        response = HttpResponse(dataset.csv, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="covid.csv"'
         return response
